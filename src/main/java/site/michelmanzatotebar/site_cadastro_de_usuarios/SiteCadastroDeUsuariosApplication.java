eb15cc4ea96f0e8b61cc6e5dd92457b73dcde7a9
michelmanzatotebar <zsouta22@gmail.com>
2026-03-08 10:43:53 -0400

    Projeto spring boot

---
+++ src/main/java/site/michelmanzatotebar/site_cadastro_de_usuarios/SiteCadastroDeUsuariosApplication.java
@@ -0,0 +1,13 @@
+package site.michelmanzatotebar.site_cadastro_de_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteCadastroDeUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteCadastroDeUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/michelmanzatotebar/site_cadastro_de_usuarios/SiteCadastroDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package site.michelmanzatotebar.site_cadastro_de_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteCadastroDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
